e531f9dc322c382c3c6f9b681004c319a5bbeebd
pfischof <pfischof@gmail.com>
2024-12-04 05:23:01 +0100

    fix: add check for adding null character termination
    Because the macOS man says this about snprintf:
    "The output is always null-terminated, unless size is 0."

--- sources/vprintf/ft_vsnprintf.c
+++ sources/vprintf/ft_vsnprintf.c
@@ -6,7 +6,7 @@
 /*   By: pfischof <pfischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:20:04 by pfischof          #+#    #+#             */
-/*   Updated: 2024/12/04 04:58:27 by pfischof         ###   ########.fr       */
+/*   Updated: 2024/12/04 05:22:06 by pfischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	ft_vsnprintf(char *output, size_t size, const char *format, va_list ap)
 	init_data(&data);
 	process_format(&data, format, ap);
 	ft_strncpy(output, data.output.content, size - sizeof(char));
-	output[size - sizeof(char)] = NULL_CHAR;
+	if (size != 0)
+		output[size - sizeof(char)] = NULL_CHAR;
 	ft_free((void **)&data.output.content);
 	return (data.output.size);
 }
