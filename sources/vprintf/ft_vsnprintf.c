6c70b5be6840056f5b3ee8e2ad0548e02b68b492
pfischof <pfischof@gmail.com>
2024-12-04 05:11:34 +0100

    fix: add null character termination

--- sources/vprintf/ft_vsnprintf.c
+++ sources/vprintf/ft_vsnprintf.c
@@ -6,7 +6,7 @@
 /*   By: pfischof <pfischof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:20:04 by pfischof          #+#    #+#             */
-/*   Updated: 2024/12/01 10:35:52 by pfischof         ###   ########.fr       */
+/*   Updated: 2024/12/04 04:58:27 by pfischof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	ft_vsnprintf(char *output, size_t size, const char *format, va_list ap)
 		return (ERROR);
 	init_data(&data);
 	process_format(&data, format, ap);
-	ft_strncpy(output, data.output.content, size);
+	ft_strncpy(output, data.output.content, size - sizeof(char));
+	output[size - sizeof(char)] = NULL_CHAR;
 	ft_free((void **)&data.output.content);
 	return (data.output.size);
 }
